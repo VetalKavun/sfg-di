b4e433b5dd3b93d6975855330cda089166b27ed1
VetalKavun <vitaliykavun@2014@gmail.com>
2020-06-22 00:37:13 +0300

    fixed property type

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -4,7 +4,7 @@ import guru.springframework.sfgdi.services.GreetingService;
 
 public class SetterInjectedController {
 
-    public GreetingService greetingService;
+    private GreetingService greetingService;
 
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
